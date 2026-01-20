f356a73cbb81233545d546f1d5730bba3c1ef43e
84hasism <84hasism@gafe.molndal.se>
2026-01-20 13:35:12 +0000

    Fixade dubbletter i koden

--- app.py
+++ app.py
@@ -2,28 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def hello():
-    return 'Hello, World!'
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
-from flask import Flask
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello():
-    return 'Hello, World!'
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
 @app.route('/')
 def hello():
     return 'Hello, Git!'
 
+if __name__ == '__main__':
+    app.run(debug=True)
