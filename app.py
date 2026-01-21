43ce41201a80a4a41c0db823bb51d0409874c5be
84hasism <84hasism@gafe.molndal.se>
2026-01-21 10:51:54 +0000

    SCRUM-5: Added welcome message to homepage

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, Code Review!'
+    return 'Welcome to the Homepage! (SCRUM-5)'
 
 if __name__ == '__main__':
     app.run(debug=True)
